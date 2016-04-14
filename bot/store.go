b03f276b6e432020eef0f7843a9d9a7002a037d6
Alex Fluter <afluter@gmail.com>
2016-04-14 23:16:11 +0800

    boltdb store backend and test
    Signed-off-by: Alex Fluter <afluter@gmail.com>

--- bot/store.go
+++ bot/store.go
@@ -1,11 +1,33 @@
 package bot
 
 import (
+	"errors"
+
 	"github.com/boltdb/bolt"
 )
 
+type StoreSpace int
+
 const (
-	Factoid = "FACTOID"
+	ROOT StoreSpace = iota
+	FACTOID
+)
+
+const (
+	root    = "ROOT"
+	factoid = "FACTOID"
+)
+
+var (
+	SpaceNames = map[StoreSpace]string{
+		ROOT:    root,
+		FACTOID: factoid,
+	}
+)
+
+var (
+	ErrDirNotFound = errors.New("Directory not found")
+	ErrKeyNotFound = errors.New("Key not found")
 )
 
 type Pair struct {
@@ -26,33 +48,123 @@ type BoltStore struct {
 	db *bolt.DB
 }
 
-func NewStore() (Store, error) {
-	db, err := bolt.Open("", 0644, nil)
+func NewStore(path string) (Store, error) {
+	db, err := bolt.Open(path, 0644, nil)
 	if err != nil {
+		return nil, err
+	}
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		for _, name := range SpaceNames {
+			_, err := tx.CreateBucketIfNotExists([]byte(name))
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		db.Close()
 		return nil, err
 	}
 	return &BoltStore{db: db}, nil
 }
 
 func (b *BoltStore) Put(key string, value []byte) error {
-	return nil
+	err := b.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(root))
+		if bucket == nil {
+			return ErrDirNotFound
+		}
+		err := bucket.Put([]byte(key), value)
+
+		return err
+	})
+	return err
 }
 
 func (b *BoltStore) Get(key string) (*Pair, error) {
-	return nil, nil
+	var val []byte
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(root))
+		if bucket == nil {
+			return ErrDirNotFound
+		}
+		v := bucket.Get([]byte(key))
+		if v != nil {
+			val = dup(v)
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	if val == nil {
+		return nil, ErrKeyNotFound
+	}
+	return &Pair{key, val}, nil
 }
 
 func (b *BoltStore) Delete(key string) error {
-	return nil
+	err := b.db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(root))
+		if bucket == nil {
+			return ErrDirNotFound
+		}
+		err := bucket.Delete([]byte(key))
+
+		return err
+	})
+	return err
 }
 
 func (b *BoltStore) Exists(key string) (bool, error) {
-	return false, nil
+	var exists bool
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(root))
+		if bucket == nil {
+			return ErrDirNotFound
+		}
+		v := bucket.Get([]byte(key))
+		if v != nil {
+			exists = true
+		}
+
+		return nil
+	})
+	if err != nil {
+		return false, err
+	}
+	return exists, nil
 }
 
 func (b *BoltStore) List() ([]*Pair, error) {
-	return nil, nil
+	pairs := make([]*Pair, 0)
+	err := b.db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte(root))
+		if bucket == nil {
+			return ErrDirNotFound
+		}
+		c := bucket.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			val := dup(v)
+			pairs = append(pairs, &Pair{string(k), val})
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return pairs, nil
 }
 
 func (b *BoltStore) Close() {
+	b.db.Close()
+}
+
+func dup(src []byte) []byte {
+	new := make([]byte, len(src))
+	copy(new, src)
+	return new
 }
---
+++ bot/store_test.go
@@ -0,0 +1,87 @@
+package bot
+
+import (
+	"bytes"
+	"testing"
+)
+
+const path = "../db/test"
+
+func TestStore(t *testing.T) {
+	store, err := NewStore(path)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	data := map[string][]byte{
+		"one":   []byte("111"),
+		"two":   []byte("222"),
+		"three": []byte("333"),
+		"four":  []byte("444"),
+	}
+
+	for k, v := range data {
+		err = store.Put(k, v)
+		if err != nil {
+			t.Error(err)
+		}
+
+		exists, err := store.Exists(k)
+		if err != nil {
+			t.Error(err)
+		}
+		if !exists {
+			t.Fail()
+		}
+
+		pair, err := store.Get(k)
+		if err != nil {
+			t.Error(err)
+		}
+		if bytes.Compare(pair.Value, v) != 0 {
+			t.Error(pair.Value, "!=", v)
+		}
+	}
+	exists, err := store.Exists("not exists")
+	if err != nil {
+		t.Error(err)
+	}
+	if exists {
+		t.Fail()
+	}
+
+	pairs, err := store.List()
+	if err != nil {
+		t.Error(err)
+	}
+	for _, p := range pairs {
+		if bytes.Compare(p.Value, data[p.Key]) != 0 {
+			t.Fail()
+		}
+	}
+
+	for k, _ := range data {
+		err = store.Delete(k)
+		if err != nil {
+			t.Error(err)
+		}
+
+		exists, err := store.Exists(k)
+		if err != nil {
+			t.Error(err)
+		}
+		if exists {
+			t.Error()
+		}
+
+		pair, err := store.Get(k)
+		if err != ErrKeyNotFound {
+			t.Error(err)
+		}
+		if pair != nil {
+			t.Fail()
+		}
+	}
+	store.Close()
+}
